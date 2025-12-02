797a249c85aaf4cf5e6f6ce37313a93e2cc82677
JaimeAndres0925 <jaime.venegas@educa.madrid.org>
2025-12-02 18:34:10 +0100

    Fase 2 del DNS

--- src/main/java/LeerCliente.java
+++ src/main/java/LeerCliente.java
@@ -10,8 +10,9 @@ import java.util.Map;
 import java.util.Optional;
 
 public class LeerCliente implements Runnable {
-    private Socket socket;
-    private Map<String, List<Registro>> diccionario;
+    private final Socket socket;
+    private final Map<String, List<Registro>> diccionario;
+    String[] partes;
 
 
     public LeerCliente(Socket socket, Map<String, List<Registro>> diccionario) {
@@ -31,26 +32,39 @@ public class LeerCliente implements Runnable {
                         out.println("Bye!");
                         break;
                     } else if (linea.startsWith("LOOKUP")) {
-                        String[] partes = linea.split("\\s+");
-                        if (partes.length == 3) {
+                         partes = linea.split("\\s+");
+                    } else if (linea.equalsIgnoreCase("LIST")) {
+                        out.println("150 Inicio listado");
+                        for (Map.Entry<String, List<Registro>> entry : diccionario.entrySet()) {
+                            String dominio = entry.getKey();
+                            for (Registro registro : entry.getValue()) {
+                                System.out.println(dominio + " " + registro.getTipo() + " " + registro.getValor());
+                            }
+                        }
+                        System.out.println("226 Fin listado");
+
+
+
+                    if (partes.length == 3) {
                             String tipo = partes[1];
                             String dominio = partes[2];
                             if (diccionario.containsKey(dominio)) {
                                 Optional<Registro> resultado = diccionario.get(dominio).stream().filter(r -> r.getTipo().equalsIgnoreCase(tipo)).findFirst();
 
                                 if (resultado.isPresent()) {
-                                    System.out.println("200 " + resultado.get().getValor());
+                                    out.println("200 " + resultado.get().getValor());
+
                                 } else {
-                                    System.out.println("404 Not Found");
+                                    out.println("404 Not Found");
                                 }
                             } else {
-                                System.out.println("404 Not Found");
+                                out.println("404 Not Found");
                             }
                         } else {
-                             System.out.println("400 Bad request");
+                             out.println("400 Bad request");
                         }
                     } else {
-                            System.out.println("400 Bad request");
+                        out.println("400 Bad request");
                     }
                 } catch (Exception e) {
                     out.println("500 Server error");
